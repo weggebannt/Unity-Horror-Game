e1412261edc52245bcba6e46b4cd93d44614ecc0
TerrorizingMuffin <terrorizingmuffin@gmail.com>
2021-07-29 15:10:02 +0200

    Movement

--- Horror-Game/Assets/Scripts/PlayerMovement.cs
+++ Horror-Game/Assets/Scripts/PlayerMovement.cs
@@ -4,5 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 12f;
+    public CharacterController controller;
+    Vector3 velocity;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
 
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
 }
