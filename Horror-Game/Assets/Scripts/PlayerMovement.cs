bc331deab2c5d02bd12c7b66257641088666fcaf
TerrorizingMuffin <terrorizingmuffin@gmail.com>
2021-07-29 20:54:34 +0200

    2

--- Horror-Game/Assets/Scripts/PlayerMovement.cs
+++ Horror-Game/Assets/Scripts/PlayerMovement.cs
@@ -12,17 +12,27 @@ public class PlayerMovement : MonoBehaviour
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     bool isGrounded;
+    public float jumpHeight = 2f;
+    Animator animator;
+
+    bool wPressed;
+    bool aPressed;
+    bool sPressed;
+    bool dPressed;
+    bool shiftPressed;
+    bool isJumping;
+    bool isSneaking;
 
     void Start()
     {
-
+        animator = GetComponent<Animator>();
     }
 
     void Update()
-    {
+    { 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
+        if(isGrounded && velocity.y < 0f)
         {
             velocity.y = -2f;
         }
@@ -34,8 +44,48 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            isJumping = true;
+        }
+
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetKeyDown("KeyCode.w"))
+        {
+            wPressed = true;
+        }
+        if (Input.GetKeyDown("KeyCode.a"))
+        {
+            aPressed = true;
+        }
+        if (Input.GetKeyDown("KeyCode.d"))
+        {
+            dPressed = true;
+        }
+        if (Input.GetKeyDown("KeyCode.s"))
+        {
+            sPressed = true;
+        }
+
+        if (wPressed && isGrounded)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        if (dPressed && isGrounded)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        if (aPressed && isGrounded)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        if (sPressed && isGrounded)
+        {
+            animator.SetBool("isWalking", true);
+        }
     }
 }
