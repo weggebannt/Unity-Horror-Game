849ec58c497ca25f3c7efdfa8b40ec6256d7b67a
TerrorizingMuffin <terrorizingmuffin@gmail.com>
2021-07-30 11:06:39 +0200

    Animations

--- Horror-Game/Assets/Scripts/PlayerMovement.cs
+++ Horror-Game/Assets/Scripts/PlayerMovement.cs
@@ -13,79 +13,141 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask groundMask;
     bool isGrounded;
     public float jumpHeight = 2f;
-    Animator animator;
-
+    public Animator anim;
+    public float walkSpeed = 10f;
+    public float sprintSpeed = 15f;
+    public float sneakSpeed = 5f;
     bool wPressed;
     bool aPressed;
     bool sPressed;
     bool dPressed;
     bool shiftPressed;
+    bool isSprinting;
     bool isJumping;
     bool isSneaking;
+    bool isWalking;
 
     void Start()
     {
-        animator = GetComponent<Animator>();
+
     }
 
     void Update()
     { 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0f)
+        if(isGrounded && velocity.y <= 0f)
         {
             velocity.y = -2f;
         }
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(x, 0f, z).normalized;
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
 
+        //Jump
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
+            anim.SetBool("isJumping", true);
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            isJumping = true;
+            isGrounded = false;
+        }
+
+        if (Input.GetButtonUp("Jump") && isGrounded)
+        {
+            anim.SetBool("isJumping", false);
         }
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
 
-        if (Input.GetKeyDown("KeyCode.w"))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             wPressed = true;
         }
-        if (Input.GetKeyDown("KeyCode.a"))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             aPressed = true;
         }
-        if (Input.GetKeyDown("KeyCode.d"))
+        if (Input.GetKeyDown(KeyCode.D))
         {
             dPressed = true;
         }
-        if (Input.GetKeyDown("KeyCode.s"))
+        if (Input.GetKeyDown(KeyCode.S))
         {
             sPressed = true;
         }
 
-        if (wPressed && isGrounded)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isGrounded && isSneaking == false && direction.magnitude >= 0.1f && (wPressed == true || aPressed == true || sPressed == true || dPressed == true))
         {
-            animator.SetBool("isWalking", true);
+            walkSpeed = sprintSpeed;
+            isSprinting = true;
+            anim.SetBool("isRunning", true);
+            isWalking = false;
         }
-        if (dPressed && isGrounded)
+        if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            animator.SetBool("isWalking", true);
+            walkSpeed = 10f;
+            isSprinting = false;
+            anim.SetBool("isRunning", false);
         }
-        if (aPressed && isGrounded)
+
+        //Sneak
+        if (Input.GetKeyDown(KeyCode.C) && isGrounded && direction.magnitude >= 0.1f)
         {
-            animator.SetBool("isWalking", true);
+            walkSpeed = sneakSpeed;
+            isSneaking = true;
+            anim.SetBool("isSneaking", true);
+            isWalking = false;
         }
-        if (sPressed && isGrounded)
+        if (Input.GetKeyUp(KeyCode.C))
         {
-            animator.SetBool("isWalking", true);
+            walkSpeed = 10f;
+            isSneaking = false;
+            anim.SetBool("isSneaking", false);
+        }
+
+        //sprint if walking
+        if (isWalking = true && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            anim.SetBool("isRunning", true);
+            anim.SetBool("isWalking", false);
+        }
+
+
+        //Sneaking if is Walking
+        if (isWalking == true && Input.GetKeyDown(KeyCode.C) && isGrounded)
+        {
+            anim.SetBool("isSneaking", true);
+            anim.SetBool("isWalking", false);
+            isSneaking = true;
+        }
+
+
+        //Sprinting if is Walking
+        if (isWalking == true && Input.GetKeyDown(KeyCode.LeftShift) && isGrounded && isSneaking == false)
+        {
+            anim.SetBool("isRunning", true);
+            anim.SetBool("isWalking", false);
+            isSprinting = true;
+        }
+
+
+        //Detecting if Player is Moving
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded && direction.magnitude > 0.1f || Input.GetKeyDown(KeyCode.S) && isGrounded && direction.magnitude > 0.1f || Input.GetKeyDown(KeyCode.A) && isGrounded && direction.magnitude > 0.1f || Input.GetKeyDown(KeyCode.D) && isGrounded && direction.magnitude > 0.1f)
+        {
+            isWalking = true;
+            anim.SetBool("isWalking", true);
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            isWalking = false;
+            anim.SetBool("isWalking", false);
         }
     }
 }
